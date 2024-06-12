69707149998ce23b5f61ce75f3e30def357e19a4
HercaiErdem <111076131+HercaiErdem@users.noreply.github.com>
2024-06-12 15:12:51 +0300

    v2

---
+++ cypress/e2e/day01/first_test2.cy.js
@@ -0,0 +1,23 @@
+/// <references types ="cypress"/>
+
+describe('My First Test', function() { // narrow funtion yerine uzun hali de kullanılabilir, bu test class'ında bunu gördük.
+    
+
+    it('Url Test', function() {
+
+        cy.visit("https://www.google.com") 
+
+       cy.url().should("include", "google")
+       
+        
+    });
+
+     it('Title Test', function() {
+        cy.visit("https://www.google.com")
+        cy.title().should("include", "Google")
+
+        cy.title().should("include", "Goo") 
+        cy.title().should("include", "le")
+        cy.title().should("eq", "Google") 
+     });
+});
